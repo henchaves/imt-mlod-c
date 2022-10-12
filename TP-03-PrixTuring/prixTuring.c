d3f078dbaed61ebf45068fb95f9dd73a6695f100
Henrique Chaves <henriquechavesmm@gmail.com>
2022-10-12 11:34:15 +0200

    add readWinners and printWinners

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -20,14 +20,15 @@
 /* This function scans a line of text (until \n) and returns a char* that contains all characters on the line (up to 255) excluding \n.
 It also ensures the \0 termination.
 **WARNING**: The result of this function has been allocated (calloc) by the function */
-char* scanLine()
+char *scanLine()
 {
 	int maxLineSize = 255;
-	char c, *line = calloc(maxLineSize+1,sizeof(char));
+	char c, *line = calloc(maxLineSize + 1, sizeof(char));
 
 	scanf("%250[^\n]", line);
 
-	if ( (c = getchar()) != '\n') {
+	if ((c = getchar()) != '\n')
+	{
 		/* we did not get all the line */
 		line[250] = '[';
 		line[251] = line[252] = line[253] = '.';
@@ -35,25 +36,55 @@ char* scanLine()
 		// line[255] = '\0'; // useless because already initialized by calloc
 
 		// let's skip all chars untli the end of line
-		while (( (c = getchar()) != '\n') && c != EOF) ;
+		while (((c = getchar()) != '\n') && c != EOF)
+			;
 	}
 
 	return line;
 }
 
 /* This function scans a line of text (until \n), converts it as an integer and returns this integer */
-int scanLineAsInt() {
+int scanLineAsInt()
+{
 	int buf;
-	scanf("%i\n",&buf);
+	scanf("%i\n", &buf);
 	return buf;
 }
 
+typedef struct
+{
+	int year;
+	char *name;
+	char *title;
+} Winner;
+
+void printWinners(Winner *winners, int numberOfWinners)
+{
+	printf("%i\n", numberOfWinners);
+	for (int i = 0; i < numberOfWinners; i++)
+	{
+		printf("%i\n%s\n%s\n", winners[i].year, winners[i].name, winners[i].title);
+	}
+}
+
+void readWinners(void)
+{
+	int numberOfWinners = scanLineAsInt();
+	Winner *winners = calloc(numberOfWinners, sizeof(Winner));
+
+	for (int i = 0; i < numberOfWinners; i++)
+	{
+		winners[i].year = scanLineAsInt();
+		winners[i].name = scanLine();
+		winners[i].title = scanLine();
+	}
+
+	printWinners(winners, numberOfWinners);
+}
 
 int main(void)
 {
-
-	int nbGagnants = scanLineAsInt();
-	printf("nbGagnants = %i\n",nbGagnants);
+	readWinners();
 
 	return EXIT_SUCCESS;
 }
