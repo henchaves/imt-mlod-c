ce48c3db71ce33daff49c86614cf612c72e47b7e
Henrique Chaves <henriquechavesmm@gmail.com>
2022-11-07 14:48:05 +0100

    Update repeat to get the array size

--- TP-Note/tpnote1.c
+++ TP-Note/tpnote1.c
@@ -3,22 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 3
-
-void repeat(int *t1, int *t2)
+void repeat(int *t1, int *t2, int size)
 {
   int count = 0;
-
-  for (int i = 0; i < SIZE; i++)
+  for (int i = 0; i < size; i++)
   {
     count += t1[i];
   }
-
   int *t3 = malloc(count * sizeof(int));
 
   int index = 0;
 
-  for (int i = 0; i < SIZE; i++)
+  for (int i = 0; i < size; i++)
   {
     for (int j = 0; j < t1[i]; j++)
     {
@@ -36,8 +32,11 @@ void repeat(int *t1, int *t2)
 
 int main()
 {
-  int t1[SIZE] = {1, 2, 4};
-  int t2[SIZE] = {10, 3, 8};
-  repeat(t1, t2);
+  int t1[] = {1, 2, 4};
+  int t2[] = {10, 3, 8};
+
+  int arraySize = sizeof t1 / sizeof t1[0];
+
+  repeat(t1, t2, arraySize);
   return 0;
 }
